4d485343ad0202aecf4a76ed2ebd73a1f60475b7
nimamovic <nimamovic3@etf.unsa.ba>
2018-10-28 21:31:38 +0100

    zavrsen zadatak

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -16,23 +16,61 @@ public class Interval {
         pocetak = false;
         kraj = false;
     }
-    public boolean isNull(Interval i){
-        if(pocetna == 0 && krajnja == 0 && pocetak == false && kraj == false) return true;
+    public boolean isNull(){
+        if(pocetna == 0 && krajnja == 0 &&  pocetak == false && kraj == false) return true;
+        return false;
     }
     public boolean isIn(double t){
-        if(t >= pocetna && t <= krajnja) return true;
+        if((t > pocetna ||  (t == pocetna && pocetak == true)) && (t < krajnja || (t == krajnja && kraj == true))) return true;
+        return false;
     }
     public Interval intersect(Interval i){
-        Interval r;
-        if(pocetna>i.pocetna && krajnja>i.krajnja && krajnja<i.pocetna){
+        Interval r = new Interval();
+        if(pocetna<i.pocetna) {
             r.pocetna=i.pocetna;
-            r.krajnja=krajnja;
+            r.pocetak=i.pocetak;
         }
-        else if(pocetna<i.pocetna && krajnja<i.krajnja && i.krajnja>pocetna){
-            r.pocetna=i.pocetna;
+        else {
+            r.pocetna=pocetna;
+            r.pocetak=pocetak;
+        }
+        if(krajnja<i.krajnja) {
             r.krajnja=krajnja;
+            r.kraj=kraj;
+        }
+        else {
+            r.krajnja=i.krajnja;
+            r.kraj=i.kraj;
+        }
+        if((pocetna>i.pocetna) || (i.pocetna>krajnja)){
+            Interval prazan=new Interval();
+            return prazan;
         }
         return r;
     }
+    public static Interval intersect(Interval i1, Interval i2){
+        return i1.intersect(i2);
+    }
 
+    @Override
+    public boolean equals(Object obj) {
+        Interval interval=(Interval) obj;
+        if (interval.pocetna == pocetna && interval.krajnja == krajnja && pocetak == interval.pocetak && kraj == interval.kraj)
+            return true;
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        if (pocetna == 0 && krajnja == 0) s = "()";
+        else {
+            if (pocetak == false) s += "(";
+            else s += "[";
+            s += pocetna + "," + krajnja;
+            if (kraj == false) s += ")";
+            else s += "]";
+        }
+        return s;
+    }
 }
